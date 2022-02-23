563f3bf456d93b124c20424bd6e19b32d60495c3
KOBAYASHI Yū <kb10uy@kb10uy.org>
2022-02-24 01:44:33 +0900

    Using simple if/else (#1)
    個人的にはこっちのほうがスッキリしている気がする

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . "/../lib/annict.php";
 require_once __DIR__ . "/../lib/myanimelist.php";
+
+$annict = Annict::from_session();
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,20 +14,19 @@ require_once __DIR__ . "/../lib/myanimelist.php";
         <h1>annict2mal</h1>
         <p>Annictの「視聴済み」作品の情報をMyAnimeListにコピーします</p>
         <p>MyAnimeList側で「視聴済み」にした作品数が1000件を越えると正しく動かないかもしれません</p>
+        <?php if ($annict === null): ?>
+            <a href="/login.php">Login with Annict</a>
+        <?php else: ?>
 <?php
-$annict = Annict::from_session();
-if ($annict == NULL) {
-    ?><a href="/login.php">Login with Annict</a><?php
-} else {
-    ?><p>Annict: @<?= $annict->username ?>でログイン中</p><?php
-    $mal = MyAnimeList::from_session();
-    if ($mal == NULL) {
-        ?><a href="/mal_login.php">Login with MyAnimeList</a><?php
-    } else {
-        ?><p>MyAnimeList: @<?= $mal->username ?>でログイン中</p><?php
-        ?><button id="start" data-csrf="<?= get_csrf_token(); ?>">同期開始</button><pre id="log"></pre><script src="/sync.js"></script><?php
-    }
-}
+            $mal = MyAnimeList::from_session();
 ?>
+            <p>Annict: @<?= $annict->username ?>でログイン中</p>
+            <?php if ($mal === null): ?>
+                <a href="/mal_login.php">Login with MyAnimeList</a>
+            <?php else: ?>
+                <p>MyAnimeList: @<?= $mal->username ?>でログイン中</p>
+                <button id="start" data-csrf="<?= get_csrf_token(); ?>">同期開始</button><pre id="log"></pre><script src="/sync.js"></script>
+            <?php endif; ?>
+        <?php endif; ?>
     </body>
 </html>
